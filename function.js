fccae2d1b7551e67ad703429ea9f7b39c792f5e3
evshadrina <113014798+evshadrina@users.noreply.github.com>
2023-10-05 14:39:14 +0300

    Add files via upload

--- function.js
+++ function.js
@@ -20,3 +20,45 @@ function find_edit_name(){
 
 }
 node_for_click_name.addEventListener("click",find_edit_name)
+
+function insert_img(){
+// Создаем элемент изображения
+const img = document.createElement('img');
+
+// Устанавливаем путь к изображению
+img.src = 'image.jpg';
+
+// Устанавливаем альтернативный текст для изображения
+img.alt = 'Описание изображения';
+img.height = '150';
+img.width='150';
+
+// Получаем контейнер, куда нужно вставить изображение
+const container = document.querySelector('#image-container');
+//const container = document.getElementById("image-container");
+// Вставляем изображение в контейнер
+container.append(img);
+
+//document.getElementsByTagName("div")[0].appendChild(img); // вставили в div 1(внизу)
+}
+const node_insert_img = document.getElementById("insert_img")
+node_insert_img.addEventListener("click",insert_img)
+
+function insert_table(){
+let table = document.querySelector('#table');
+
+for (let i = 0; i < 3; i++) {
+	let tr = document.createElement('tr');
+
+	for (let i = 0; i < 3; i++) {
+		let td = document.createElement('td');
+		tr.appendChild(td);
+		td.innerText="AAA"
+	}
+
+	table.appendChild(tr);
+}
+}
+
+const node_insert_table = document.getElementById("insert_table")
+node_insert_table.addEventListener("click",insert_table)
